b9ce6b1eaaa4aa5156a3545b06304573c4ca29ef
Ubuntu <jcortes@linux-vm.oziqepeb4yde5ai0zxn04kua1e.jx.internal.cloudapp.net>
2023-05-09 23:35:11 +0000

    fixed vSWitch creation

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "alicloud_vswitch" "global_slave" {
   vpc_id       = var.global_vpc_id
   cidr_block   = var.global_vswitch_cidr_slave_cen_tr
   #cidr_block   = cidrsubnet(var.global_vpc_cidr, 5, 5)
-  zone_id      = var.cen_instance_id != null ? data.alicloud_cen_transit_router_available_resources.global_existing[0].resources[0].slave_zones[0] : data.alicloud_cen_transit_router_available_resources.global_new[0].resources[0].slave_zones[0]
+  zone_id      = var.cen_instance_id != null ? data.alicloud_cen_transit_router_available_resources.global_existing[0].resources[0].slave_zones[0] : data.alicloud_cen_transit_router_available_resources.global_new[0].resources[0].slave_zones[1]
 }
 
 # 1e. Create vSwitch for Transit Router in China Master Zone
@@ -84,7 +84,7 @@ resource "alicloud_vswitch" "china_slave" {
   vpc_id       = var.china_vpc_id
   cidr_block   = var.china_vswitch_cidr_slave_cen_tr
   #cidr_block   = cidrsubnet(var.china_vpc_cidr, 5, 5)
-  zone_id      = var.cen_instance_id != null ? data.alicloud_cen_transit_router_available_resources.china_existing[0].resources[0].master_zones[0] : data.alicloud_cen_transit_router_available_resources.china_new[0].resources[0].master_zones[0]
+  zone_id      = var.cen_instance_id != null ? data.alicloud_cen_transit_router_available_resources.china_existing[0].resources[0].master_zones[0] : data.alicloud_cen_transit_router_available_resources.china_new[0].resources[0].slave_zones[1]
 }
 
 
@@ -116,11 +116,11 @@ resource "alicloud_cen_transit_router_vpc_attachment" "global" {
 
   zone_mappings {
     vswitch_id = alicloud_vswitch.global_master.id
-    zone_id    = var.cen_instance_id != null ? data.alicloud_cen_transit_router_available_resources.global_existing[0].resources[0].master_zones[0] : data.alicloud_cen_transit_router_available_resources.global_new[0].resources[0].master_zones[0]
+    zone_id    = alicloud_vswitch.global_master.zone_id
   }
   zone_mappings {
     vswitch_id = alicloud_vswitch.global_slave.id
-    zone_id    = var.cen_instance_id != null ? data.alicloud_cen_transit_router_available_resources.global_existing[0].resources[0].slave_zones[0] : data.alicloud_cen_transit_router_available_resources.global_new[0].resources[0].slave_zones[0]
+    zone_id    = alicloud_vswitch.global_slave.zone_id
   }  
 }
 
@@ -134,11 +134,11 @@ resource "alicloud_cen_transit_router_vpc_attachment" "china" {
 
   zone_mappings {
     vswitch_id = alicloud_vswitch.china_master.id
-    zone_id    = var.cen_instance_id != null ? data.alicloud_cen_transit_router_available_resources.china_existing[0].resources[0].master_zones[0] : data.alicloud_cen_transit_router_available_resources.china_new[0].resources[0].master_zones[0]
+    zone_id    = alicloud_vswitch.china_master.zone_id
   }
   zone_mappings {
     vswitch_id = alicloud_vswitch.china_slave.id
-    zone_id    = var.cen_instance_id != null ? data.alicloud_cen_transit_router_available_resources.china_existing[0].resources[0].slave_zones[0] : data.alicloud_cen_transit_router_available_resources.china_new[0].resources[0].slave_zones[0]
+    zone_id    = alicloud_vswitch.china_slave.zone_id
   }
 }
 

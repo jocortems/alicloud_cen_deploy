5b0cbff5d8c32e4dd409fdb50cdc49f0ef2b3aeb
Ubuntu <jcortes@linux-vm.oziqepeb4yde5ai0zxn04kua1e.jx.internal.cloudapp.net>
2023-05-10 15:48:27 +0000

    Fixed slave zone for China vSwitch

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "alicloud_vswitch" "china_slave" {
   vpc_id       = var.china_vpc_id
   cidr_block   = var.china_vswitch_cidr_slave_cen_tr
   #cidr_block   = cidrsubnet(var.china_vpc_cidr, 5, 5)
-  zone_id      = var.cen_instance_id != null ? data.alicloud_cen_transit_router_available_resources.china_existing[0].resources[0].master_zones[1] : data.alicloud_cen_transit_router_available_resources.china_new[0].resources[0].slave_zones[1]
+  zone_id      = var.cen_instance_id != null ? data.alicloud_cen_transit_router_available_resources.china_existing[0].resources[0].slave_zones[1] : data.alicloud_cen_transit_router_available_resources.china_new[0].resources[0].slave_zones[1]
 }
 
 
